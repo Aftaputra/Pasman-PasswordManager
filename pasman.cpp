0290b37906e54f83976b4337727f0ee9cd5d6608
Afta <79725341+Aftaputra@users.noreply.github.com>
2023-11-12 09:29:34 +0700

    Revisi encoding ascii to base64
    ciphertext that has been xor-ed should be converted to base64 format so it can be stored into txt without losing some of its characters

--- pasman.cpp
+++ pasman.cpp
@@ -3,93 +3,84 @@
 #include <fstream>
 #include <stdio.h>
 #include <Windows.h>
-
-//data = jemput txt
-//myfile = simpan txt
+#include "base64.cpp"
 using namespace std;
 
-
 string Encrypt(string password, string key){
-    // used to convert string to integer of the key
-    // used as temporary variable for the converted password
-    unsigned char pass;
     for(int i = 0; i < password.length(); i++){
-        pass = (unsigned char)((unsigned char)password[i] ^ (unsigned char)key[i % (key.length())]);
-        password[i] = (unsigned char)(pass) + 128;
-    }
-
+        password[i] = (char)password[i] ^ key[i % (key.length() + 1)];
+    } password = base64_encode(password);
     return password;
 }
-
-string Decypt(string password, string key){
-    // used to convert string to integer of the key
-    // used as temporary variable for the converted password
-    unsigned char pass;
+string Decrypt(string password, string key){
+    password = base64_decode(password);
     for(int i = 0; i < password.length(); i++){
-        pass = (unsigned char)((unsigned char)(password[i] - 128) ^ (unsigned char)key[i % (key.length())]);
-        password[i] = (unsigned char)(pass);
-    }
-
+        password[i] = (char)password[i] ^ key[i % (key.length() + 1)];
+    } 
     return password;
 }
 
 void DisplayAll(string key) {
     string content[10000][2];
-    ifstream data("data.txt"); //deklarasi file
-    //jika file pass.txt bisa terbuka,
+    ifstream data("data.txt", ios::in); 
     if(data.is_open()){
-        //masukkan isi pass.txt ke array content[][]
-        // nanti forloops diganti while sampai EOF
         int i = 0;
         while(true){
             if(data.eof()){
                 break;
+            } 
+            cout << i + 1 << ".";
+            for(int j = 0; j < 2; j++){
+                data >> content[i][j];
+                cout << Decrypt(content[i][j], key) << " ";
             }
-            else {
-                cout << i + 1 << ".";
-                for(int j = 0; j < 2; j++){
-                    data >> content[i][j];
-                     
-                    cout << Decypt(content[i][j], key) << " ";
-                    // cout << content[i][j] << " ";
-                }
-                cout << endl;
-                i++;
-            }
+            cout << endl;
+            i++;
         } 
     }
-
     data.close();
 }
 
+void GetPassword(int nomor) {
+    string content[10000][2];
+    ifstream data("data.txt"); 
+    if(data.is_open()){
+        //masukkan isi pass.txt ke array content[][]
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 2; j++){
+                data >> content[i][j];
+            }
+        }
+    }
+    data.close();
+    cout << content[nomor + 1][1];
+}
+
 void Saving(string saved[]) {
-    ofstream myfile("data.txt", fstream::app);
+    ofstream myfile("data.txt", ios::out| fstream::app);
+    
     if(myfile.is_open()){     
         for(int j = 0; j < 2; j++){ //inner untuk 2 input akun & pass
             myfile << saved[j] << " ";
-        }
+        } 
         myfile << endl;
-        
     }
     else {
         cout << "File tidak dapat dibuka/ditemukan";
     }
-
     myfile.close();
 }
 
-string Generate(int panjang){
-    const string cha = "1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:ZXCVBNM<>?";
-    string pw;
-    for (int i=0;i<panjang;i++){
-        int chpw= rand() % cha.size();
-        pw += cha[chpw];
-        
-    }
-
-    return pw;
+string generate(int panjang){
+	const string cha = "1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:ZXCVBNM<>?";
+	string pw;
+	for (int i=0;i<panjang;i++){
+		int chpw= rand() % cha.size();
+		pw += cha[chpw];
+		
+	}
+	return pw;
 }
-
 
 int main() {
     
@@ -105,13 +96,13 @@ int main() {
     cout << endl;
     cout << endl;
     while(true){
-        cout << "Silakan pilih fitur yang diinginkan : " << endl;
-        cout << "1. Input Manual Password" << endl << "2. Auto-generated Password" << endl << "3. Look-up saved password" << endl << "4. Exit" ;
-        cout << endl;
-        
-        int x;
-        cout << "Masukkan angka (1 - 4) = ";
-        cin >> x;
+    cout << "Silakan pilih fitur yang diinginkan : " << endl;
+    cout << "1. Input Manual Password" << endl << "2. Auto-generated Password" << endl << "3. Look-up saved password" << endl << "4. Exit" ;
+    cout << endl;
+    
+    int x;
+    cout << "Masukkan angka (1 - 4) = ";
+    cin >> x;
 
         if(x == 1){
             // //input manual
@@ -124,10 +115,12 @@ int main() {
                 cin >> InputAcc[i];
                 InputAcc[i] = Encrypt(InputAcc[i], key);
             }
+            
             Saving(InputAcc);
             cout << "Data berhasil disimpan!" << endl;
             cout << endl;
             Sleep(1000);
+            system("CLS");
         }
         else if(x == 2){
             //generate random
@@ -140,16 +133,14 @@ int main() {
             cin >> InputAcc[0];
             cout << "Masukkan Panjang Password= ";
             cin >> ln;
-            InputAcc[1] = Generate(ln);
-            cout << "Password: " << InputAcc[1] << endl; 
+            InputAcc[1] = generate(ln);
             for(int i = 0; i < 2; i++){
                 InputAcc[i] = Encrypt(InputAcc[i], key);
             }
             Saving(InputAcc);
-            cout << "Password: " << InputAcc[1] << endl; 
             cout << "Data berhasil disimpan!" << endl;
             cout << endl;
-
+            system("CLS");
         }
         else if(x == 3){
             //lookup
@@ -164,9 +155,12 @@ int main() {
             cout << "Terimakasih!";
             cout << endl;
             break;
-        } 
-        else {
-            cout << "Masukkan angka sesuai opsi(1-4) yang Benar";
+        }else if(x == 0){
+            cout << "Masukkan yg bener";
+            cout << endl;
+            continue;
+        } else {
+            cout << "Masukkan yg bener";
             cout << endl;
             continue;
         }
